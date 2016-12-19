d42941841d0ef33aaed5e239c0ea3fae25fc3214
Padmate <959853840@qq.com>
2016-12-19 17:41:16 +0800

    测试

--- resources/views/Home/index.php
+++ resources/views/Home/index.php
@@ -1,8 +1,8 @@
 <form action="/account/login" method="post">
     <input type="text" name="name" value="Allen">
-    <input type="submit" value="提交">
+    <input type="submit" value="提交aa">
 </form>
-<input type="button" value="ajax提交" class="ajax">
+<input type="button" value="ajax提交bb" class="ajax">
 <script src="../../js/jquery-1.10.2.min.js"></script>
 <script>
     $(".ajax").click(function(){
